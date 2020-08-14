23abc62a29ec4606099414681f180b0e660e0639
hsrv <ahusrevceker@gmail.com>
2020-08-14 17:09:46 +0300

    Add files via upload

---
+++ leave_a_space.py
@@ -0,0 +1,35 @@
+text = """your text here"""
+
+
+text = text.split(".")
+
+text = ". ".join(map(str, text))
+
+text = text.split(",")
+
+text = ", ".join(map(str, text))
+
+print(text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
