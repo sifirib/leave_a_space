0d77a919ff30280ecc3e660ab3333723cf21983c
hsrv <ahusrevceker@gmail.com>
2020-09-30 03:04:17 +0300

    Update leave_a_space.py

--- leave_a_space.py
+++ leave_a_space.py
@@ -1,35 +1,6 @@
 text = """your text here"""
-
-
 text = text.split(".")
-
 text = ". ".join(map(str, text))
-
 text = text.split(",")
-
 text = ", ".join(map(str, text))
-
 print(text)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
